4ecf5d6c5d9bd6ef78b56f1415d08015a8d7fa5b
Nishant Singh <49143069+nishant541@users.noreply.github.com>
2019-04-22 12:33:28 +0530

    Add files via upload

---
+++ 1assignmnt.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>1assignment</title>
+<style type="text/css">
+
+	body{
+			background: #d7e7f4;
+
+		}
+
+		
+		*{
+			margin: 0px;
+			padding: 0px;
+		}
+	.main{
+			width: 954px;
+			height: 657px;
+			background:#fff;
+			margin: 0 auto;
+		}
+	.header{
+		height: 100px;
+		width: 100%;
+		background-color:#fff;
+	}
+	.logo{
+		height: 100px;
+		width: 230px;
+		background-color: #fff;
+		padding: 22px;
+		float: left;
+
+	}
+	.right-block{
+		height: 100px;
+		width: 300px;
+		background-color: #fff;
+		float: right;
+}
+	.login
+	{
+		height: 11px;
+		width: 225px;
+		margin: 22px;
+		text-align: right;
+		text-decoration: underline;
+		font-family:sans-serif;
+		font-weight: bold;
+		margin-left: 22px: 
+	}
+	.right-block input[type="submit"]{
+    text-transform: uppercase;
+    background: #007BAF;
+    padding: 4px 8px;
+    font-size: 11px;
+    color: #fff;
+    border: 1px solid transparent;
+}
+	.search-form form{
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+   
+}
+
+.content{
+				width: 100%;
+				height: 250px;
+				background:#fff;
+		}
+.footer{
+			width: 100%;
+			height: 6px;
+			background:#000;
+
+		}
+		
+</style>
+
+
+</head>
+<body>
+	<div class="main">
+		<div class="header">
+			<div class="logo"><img src="logo.png"></div>
+			<div class="right-block">
+				<div class="login"><a href="#"> <p style="color: #007BAF;">Login to my Account</p></a></div>
+						<div class="search-form">
+                            <form action="#">
+                                <input type="text" name="text" id="search" placeholder="Search entire store here...">&nbsp;&nbsp;
+                                <input type="submit" value="search">
+                            </form>
+                        </div>			
+			</div>
+			</div>
+<div class="content"></div>
+<div class="footer"></div>
+
+		</div>
+
+
+
+	</div>
+
+</body>
+</html>
